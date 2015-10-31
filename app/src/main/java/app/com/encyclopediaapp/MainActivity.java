f86b0e5b36cc9f2b91214390fcc39c9b6055ec32
Faris Abu Saleem <faris.it.cs@gmail.com>
2015-10-31 13:07:50 +0200

    test

--- app/src/main/java/app/com/encyclopediaapp/MainActivity.java
+++ app/src/main/java/app/com/encyclopediaapp/MainActivity.java
@@ -1,16 +1,41 @@
 package app.com.encyclopediaapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
 
+    @Bind(R.id.imageButton) ImageButton numberIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        numberIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent goToNumberActivity = new Intent(MainActivity.this,
+                        NumbersActivity.class);
+                startActivity(goToNumberActivity);
+
+                // Github
+
+            }
+        });
+
+
+
     }
 
 }
--- app/src/main/java/app/com/encyclopediaapp/SplashScreenActivity.java
+++ app/src/main/java/app/com/encyclopediaapp/SplashScreenActivity.java
@@ -1,26 +1,44 @@
 package app.com.encyclopediaapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class SplashScreenActivity extends Activity {
+
+
+
+    @Bind(R.id.background_img) ImageView myImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ButterKnife.bind(this);
+
+      Runnable goToMain = new Runnable() {
+          @Override
+          public void run() {
+
+              //myImage.setImageResource(R.drawable.coffee_1);
+
+              Intent mainActivity = new
+                      Intent(SplashScreenActivity.this,MainActivity.class);
+              startActivity(mainActivity);
 
 
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
+          }
+      };
 
-            } 
-        };
+        new android.os.Handler().postDelayed(goToMain,3000);
 
-        new android.os.Handler().postDelayed(runnable,3000);
+
     }
 
 }
