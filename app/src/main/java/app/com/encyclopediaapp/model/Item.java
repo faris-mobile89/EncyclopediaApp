79316552bb50f5cfbd272f4f7d29e18fd6dd5f29
Faris Abu Saleem <faris.it.cs@gmail.com>
2015-11-07 11:20:26 +0200

    master

--- app/src/main/java/app/com/encyclopediaapp/model/Item.java
+++ app/src/main/java/app/com/encyclopediaapp/model/Item.java
@@ -4,6 +4,12 @@ package app.com.encyclopediaapp.model;
 public class Item {
 
     private String id;
+    private String text;
+    private String voiceUrl;
+    private String imageUrl;
+
+    public Item() {
+    }
 
     public String getId() {
         return id;
@@ -11,5 +17,29 @@ public class Item {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getVoiceUrl() {
+        return voiceUrl;
+    }
+
+    public void setVoiceUrl(String voiceUrl) {
+        this.voiceUrl = voiceUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
